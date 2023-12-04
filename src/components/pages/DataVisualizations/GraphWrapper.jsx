6707d255e7cc6ee3d24cc8bfe974584e2e96d0df
Logan <thelogan2019@gmail.com>
2023-12-04 13:46:39 -0600

    graphs are working

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,7 +10,6 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
@@ -50,60 +49,63 @@ function GraphWrapper(props) {
         break;
     }
   }
-  function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
-          _                                                                             _
-        |                                                                                 |
-        |   Example request for once the `/summary` endpoint is up and running:           |
-        |                                                                                 |
-        |     `${url}/summary?to=2022&from=2015&office=ZLA`                               |
-        |                                                                                 |
-        |     so in axios we will say:                                                    |
-        |                                                                                 |     
-        |       axios.get(`${url}/summary`, {                                             |
-        |         params: {                                                               |
-        |           from: <year_start>,                                                   |
-        |           to: <year_end>,                                                       |
-        |           office: <office>,       [ <-- this one is optional! when    ]         |
-        |         },                        [ querying by `all offices` there's ]         |
-        |       })                          [ no `office` param in the query    ]         |
-        |                                                                                 |
-          _                                                                             _
-                                   -- Mack 
-    
-    */
-
+  async function updateStateWithNewData(
+    years,
+    view,
+    office,
+    stateSettingCallback
+  ) {
     if (office === 'all' || !office) {
-      axios
-        .get(`${process.env.REACT_APP_API_URI}/`, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+      //If office is "all" or not provided, fetch data without specifying the office parameter
+      const fiscal = await axios.get(
+        `${process.env.REACT_APP_API_URI}/fiscalSummary`,
+        {
           params: {
             from: years[0],
             to: years[1],
           },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
+        }
+      );
+
+      const citizenship = await axios.get(
+        `${process.env.REACT_APP_API_URI}/citizenshipSummary`,
+        {
+          params: {
+            from: years[0],
+            to: years[1],
+          },
+        }
+      );
+
+      fiscal.data.citizenshipResults = citizenship.data;
+      //combines the fiscal and citizenship results by placing the citizenship data in fiscal.data.citizenshipResults
+      stateSettingCallback(view, office, [fiscal.data]);
+      //calls the callback function passing in the newly combined data
     } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+      const fiscal = await axios.get(
+        `${process.env.REACT_APP_API_URI}/fiscalSummary`,
+        {
           params: {
             from: years[0],
             to: years[1],
             office: office,
           },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
+        }
+      );
+      const citizenship = await axios.get(
+        `${process.env.REACT_APP_API_URI}/citizenshipSummary`,
+        {
+          params: {
+            from: years[0],
+            to: years[1],
+            office: office,
+          },
+        }
+      );
+      fiscal.data.citizenshipResults = citizenship.data;
+      //combines the fiscal and citizenship results by placing the citizenship data in fiscal.data.citizenshipResults
+      stateSettingCallback(view, office, [fiscal.data]);
+      //calls the callback function passing in the newly combined data
     }
   }
   const clearQuery = (view, office) => {
