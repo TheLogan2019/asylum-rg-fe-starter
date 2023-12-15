c0c01a120522e60cbb415c60136cf27b0c07d596
Logan <thelogan2019@gmail.com>
2023-12-15 12:04:04 -0600

    Finishing touches on profile info and styling

--- src/components/common/ProfilePage.jsx
+++ src/components/common/ProfilePage.jsx
@@ -2,13 +2,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function ProfilePage() {
   const { user } = useAuth0();
-  const { name, picture, email } = user;
+  const { picture, email } = user;
+
+  const displayName = email.split('@')[0];
 
   return (
-    <div>
+    <div className="profileStats">
       <img src={picture} alt="Profile Pic" />
-      <h2>{name}</h2>
-      <h2>{name}</h2>
+      <h2>{displayName}</h2>
       <p>{email}</p>
     </div>
   );
